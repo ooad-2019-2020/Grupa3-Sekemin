6c7c847bf46ee47c1205716d60d126ae35ead9dc
Semir Suljević <ssuljevic1@etf.unsa.ba>
2020-05-03 20:44:22 +0200

    Test pushanja koda

---
+++ Implementacija - Test/Migrations/20200503090531_PetaMigracija.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Take1.Migrations
+{
+    public partial class PetaMigracija : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    Discriminator = table.Column<string>(nullable: false),
+                    HireDate = table.Column<DateTime>(nullable: true),
+                    EnrollmentDate = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Person");
+        }
+    }
+}
---
+++ Implementacija - Test/Models/OOADContext.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Take1.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Take1.Models
+{
+    public class OOADContext:DbContext
+    {
+        public OOADContext(DbContextOptions<OOADContext> options): base(options)
+        {
+        }
+        
+       public DbSet<Person> Persons { get; set; }
+       public DbSet<Instructor> Instructors { get; set; }
+       public DbSet<Student> Students { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Person>().ToTable("Person");
+           
+        }
+
+
+    }
+}
