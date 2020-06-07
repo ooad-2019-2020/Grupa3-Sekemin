a0f59f0a8ea1ea59804375dc99df3c720ef534df
Semir Suljević <ssuljevic1@etf.unsa.ba>
2020-06-07 20:35:29 +0200

    Promjena

--- Implementacija - API/Implementacija/Sekemin/Controllers/StudentController.cs
+++ Implementacija - API/Implementacija/Sekemin/Controllers/StudentController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,6 +10,7 @@ using Sekemin.Data;
 using Sekemin.Models;
 
 namespace Sekemin.Controllers {
+    [Authorize(Roles ="Student")]
     public class StudentController : Controller {
         private readonly BazaContext context;
         private readonly UserManager<Osoba> userManager;
